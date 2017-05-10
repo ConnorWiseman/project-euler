bb4e7d4f59e1ed904d05f538ddaa75da0dbe6f11
Connor Wiseman <wiseman.connor@gmail.com>
2017-05-10 07:56:05 -0600

    Indentation

--- php/001.php
+++ php/001.php
@@ -1,15 +1,15 @@
 <?php
 
 function sumMultiplesOfThreeAndFiveBelow($n) {
-  $sum = 0;
+    $sum = 0;
 
-  for ($i = 0; $i < $n; $i++) {
-    if ($i % 3 === 0 || $i % 5 === 0) {
-      $sum += $i;
+    for ($i = 0; $i < $n; $i++) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
     }
-  }
 
-  return $sum;
+    return $sum;
 }
 
 $answer = sumMultiplesOfThreeAndFiveBelow(1000);
